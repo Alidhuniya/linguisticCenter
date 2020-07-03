e62a0c2f2afd3f6796144a41a8c0ca10887ce55f
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-07-02 19:52:25 -0700

    set innerHTML property null problem

--- js/modules/nav.js
+++ js/modules/nav.js
@@ -1,18 +1,25 @@
 export const navBar = async () => {
+    try {
     const url = "https://alidhuniya.github.io/linguisticCenter/lang.json";
     const data = await fetch (url);
     const res = await data.json();
     const result = res;
     console.log(result.navBar);
-    const leftNav = document.querySelector(".header__leftNav").innerHTML = ` 
+
+    document.querySelector(".header__leftNav").innerHTML = ` 
    <div class="header__logo"> 
     <div>
     <img class="header__logoImg" src="${result.navBar[0].logoImg}" alt=""> 
     </div>
    </div> 
     `;
-    const leftNavHeading = document.querySelector(".header__h1").innerHTML = ` 
-    ${result.navBar[0].logoHeading}
-    `;
-   
+ 
+    document.querySelector(".header__h1").innerHTML = ` ${result.navBar[0].logoHeading} `;
+
+}
+
+catch (error) {
+    console.log("failed", error);
+}
+    
 }
