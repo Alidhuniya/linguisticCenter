52e5a0c30e7487d6b2388650f6f60e839b02aece
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-07-07 20:22:24 -0700

    formated files with prettier

--- js/modules/section14Trust.js
+++ js/modules/section14Trust.js
@@ -1,35 +1,27 @@
 export const Trust = async () => {
-    try {
-        const url = "https://alidhuniya.github.io/linguisticCenter/lang.json";
-        const data = await fetch (url);
-        const res = await data.json();
-        const result = res;
-        // console.log(result.scheduleList[0].heading)
+  try {
+    const url = "https://alidhuniya.github.io/linguisticCenter/lang.json";
+    const data = await fetch(url);
+    const res = await data.json();
+    const result = res;
+    // console.log(result.scheduleList[0].heading)
 
-        document.querySelector(".container14__h1").innerHTML = `
+    document.querySelector(".container14__h1").innerHTML = `
         ${result.trustedHeading}
         `;
 
-        let output = '';
+    let output = "";
 
-        for (var i in result.trustedPartners) {
-            output += `
+    for (var i in result.trustedPartners) {
+      output += `
             
            <img class="container14__imgs" src="${result.trustedPartners[i].img}" alt="trustedPartners">
             `;
-        }
-        const courses = document.querySelector(".container14__companies").innerHTML = output;
-              
-        
-       
-
     }
-
-
-    catch(error) {
-        console.log("Failed", error);
-    }
-}
-
-
-
+    const courses = (document.querySelector(
+      ".container14__companies"
+    ).innerHTML = output);
+  } catch (error) {
+    console.log("Failed", error);
+  }
+};
